2621b27fb66d6ab8396182a04ed0134e8648149b
unknown <meus13@gmail.com>
2023-04-02 18:37:52 +0200

    Funkcja dlugosci hasla wprowadzenie

--- password_generator/src/main.rs
+++ password_generator/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, process};
 #[derive(Debug)]
 struct PasswordProperties{
     lenght:i32,
@@ -15,14 +15,26 @@ impl PasswordProperties{
         PasswordProperties { lenght, kind }
     }
 }
-fn main() {
+fn input_lenght()->i32{
+    println!("Podaj długość hasła: ");
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
-            println!("Podany login to{}",input);
+            println!("Podana długość to: {}",input);
+            match input.trim().parse(){ // !pamiętaj musi być trim bo inaczej występuje błąd, że bierze enter jako znak (tak obstawiam)
+                Ok(input) => input,
+                Err(_e) => {println!("Error_1 {}",_e);
+            process::exit(1)},
+            }   
         }
-        Err(error) => println!("error: {error}"),
-    }
+        Err(error) =>{println!("Error {}",error);
+        process::exit(1);},
+        }
+        
+    
+}
+fn main() {
+    let lenght=input_lenght();
     let passprop=PasswordProperties::new(32, PasswordKind::Letter);
     println!("{:?}",passprop);
     
